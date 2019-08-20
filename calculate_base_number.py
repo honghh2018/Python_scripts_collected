7b636d875224cb832607eaea2a6cd6090c08286d
Alsace <41668708+honghh2018@users.noreply.github.com>
2019-08-20 14:13:34 +0800

    Create calculate_base_number.py

---
+++ calculate_base_number.py
@@ -0,0 +1,23 @@
+#encoding:utf-8
+#!/usr/bin/env python
+import re,sys,os
+import time
+
+BaseCounts = {}
+with open(sys.argv[1]) as IN:
+    for line in IN:
+	if line.startswith(">"):
+		line=line.replace('\n','').rstrip().strip('>')
+		temp=line.split()
+		BaseCounts[temp[0]]=''
+	else:
+		line=line.replace('\n','').rstrip('\n')
+		BaseCounts[temp[0]] +=line.upper()
+
+sys.stdout.write('Gene_ID\t'+'A\t'+'C\t'+'G\t'+'T'+'\n')
+for key in sorted(BaseCounts.keys()):
+	ntCounts=[]
+	for nt in ['A','C','G','T']:
+    		ntCounts.append(BaseCounts[key].count(nt))
+	mystr='\t'.join(map(str, ntCounts))
+	sys.stdout.write(key+'\t'+mystr+'\n')
